2487f4ec5866610ab283bc0704b8b468c1fc44e8
Юрий Карачевцев <grammidin4eg@gmail.com>
2024-01-03 16:13:53 +0300

    состояние приложения

--- SDL Bird/SDL Bird.cpp
+++ SDL Bird/SDL Bird.cpp
@@ -7,11 +7,21 @@ int main()
     // константы
     const int SCREEN_HEIGHT = 800;
     const int SCREEN_WIDTH = 600;
+
+    const float GROUND_SPEED = 0.01f;
+    const float GRAVITY = 0.05f;
+    const float JUMP_POWER = 10;
+
+    enum class EGameState { Menu, Game, GameOver};
+
     // создаЄм окно
     RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SDL Bird");
     Image imgIcon;
     imgIcon.loadFromFile("Images/mainico.png");
     window.setIcon(32, 32, imgIcon.getPixelsPtr());
+
+    // размер окна
+    Vector2f windowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     // загрузка текстур
 
@@ -19,13 +29,8 @@ int main()
     Texture texBackground;
     texBackground.loadFromFile("Images/background.png");
 
-    Sprite sBackground;
-    sBackground.setTexture(texBackground);
-    Vector2f windowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    sBackground.setScale(
-        windowSize.x / sBackground.getLocalBounds().width,
-        windowSize.y / sBackground.getLocalBounds().height);
+    RectangleShape shapeBackground(windowSize);
+    shapeBackground.setTexture(&texBackground);
 
     // земл€
     Texture texGround;
@@ -33,11 +38,11 @@ int main()
 
     Sprite sGround1;
     sGround1.setTexture(texGround);
-    sGround1.setPosition(0, SCREEN_HEIGHT - sGround1.getLocalBounds().height);
+    sGround1.setScale(windowSize.x / sGround1.getLocalBounds().width, 1);
 
     Sprite sGround2;
     sGround2.setTexture(texGround);
-    sGround2.setPosition(sGround2.getLocalBounds().width, SCREEN_HEIGHT - sGround2.getLocalBounds().height);
+    sGround2.setScale(windowSize.x / sGround2.getLocalBounds().width, 1);
 
     // главное меню
     Texture texMessage;
@@ -52,6 +57,31 @@ int main()
 
     sMessage.setPosition(sMessage.getGlobalBounds().width / 2, sMessage.getGlobalBounds().height / 2);
 
+    // ѕтица
+    Texture texBirdMid;
+    texBirdMid.loadFromFile("Images/yellowbird-midflap.png");
+
+    Sprite sBird;
+    sBird.setTexture(texBirdMid);
+    sBird.setPosition(100, SCREEN_HEIGHT / 2);
+
+
+    // сообщение »гра окончена
+    Texture texGameOver;
+    texGameOver.loadFromFile("Images/gameover.png");
+
+    Sprite sGameOver;
+    sGameOver.setTexture(texGameOver);
+    sGameOver.setPosition(0, SCREEN_HEIGHT / 3);
+    sGameOver.setScale(
+        windowSize.x / sGameOver.getLocalBounds().width / 2,
+        1);
+
+
+    EGameState curState = EGameState::Menu;
+
+    // смещение скролла
+    float scrollingOffset = 0;
 
     // цикл игры
     while (window.isOpen())
@@ -62,16 +92,68 @@ int main()
         {
             if ((event.type == Event::Closed) || (Keyboard::isKeyPressed(Keyboard::Escape)))
                 window.close();
+
+            if (Keyboard::isKeyPressed(Keyboard::Space))
+            {
+                switch (curState)
+                {
+                case EGameState::Menu:
+                    curState = EGameState::Game;
+                    break;
+                case EGameState::Game:
+                    break;
+                case EGameState::GameOver:
+                    break;
+                default:
+                    break;
+                }
+            }
         }
 
         // обновление объектов
+        if (curState == EGameState::Game)
+        {
+            scrollingOffset-=GROUND_SPEED;
+            if (scrollingOffset < -sGround1.getGlobalBounds().width)
+            {
+                scrollingOffset = 0;
+            }
+            sGround1.setPosition(scrollingOffset, SCREEN_HEIGHT - sGround1.getGlobalBounds().height);
+            sGround2.setPosition(scrollingOffset + sGround2.getGlobalBounds().width, SCREEN_HEIGHT - sGround2.getGlobalBounds().height);
+
+
+            sBird.move(0, GRAVITY);
+
+            if (sBird.getPosition().y > (SCREEN_HEIGHT - sGround1.getGlobalBounds().height - sBird.getGlobalBounds().height))
+            {
+                curState = EGameState::GameOver;
+                printf("Game Over");
+            }
+        }
 
         // отрисовка
         window.clear();
-        window.draw(sBackground);
-        window.draw(sMessage);
-        window.draw(sGround1);
-        window.draw(sGround2);
+        // window.draw(sBackground);
+        window.draw(shapeBackground);
+        switch (curState)
+        {
+        case EGameState::Menu:
+            window.draw(sMessage);
+            break;
+        case EGameState::Game:
+            window.draw(sBird);
+            window.draw(sGround1);
+            window.draw(sGround2);
+            break;
+        case EGameState::GameOver:
+            window.draw(sBird);
+            window.draw(sGround1);
+            window.draw(sGround2);
+            window.draw(sGameOver);
+            break;
+        default:
+            break;
+        }
         window.display();
     }
 
